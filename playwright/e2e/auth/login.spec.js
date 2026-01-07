8101de4a53b584a4b49eaff702d490ca2cf228e6
Edson <edsonj82@outlook.com>
2026-01-06 22:40:06 -0300

    refactoring with clean code in scenario 'it should not login with invalid credentials'

--- playwright/e2e/auth/login.spec.js
+++ playwright/e2e/auth/login.spec.js
@@ -20,6 +20,7 @@ test.describe('POST /auth/login', () => {
         expect(response.status()).toBe(200);
 
         const body = await response.json();
+
         expect(body).toHaveProperty('message', 'Login realizado com sucesso')
         expect(body.data).toHaveProperty('token')
         expect(body.data).toHaveProperty('user')
@@ -29,12 +30,22 @@ test.describe('POST /auth/login', () => {
         expect(body.data.user).not.toHaveProperty('password')
     })
 
-    // test('it should not login with invalid credentials', async ({ request }) => {
+    test('it should not login with invalid credentials', async ({ request }) => {
+        //preparação
+        const user = getUser()
 
-    //     const response = await login.auth({ email: '<EMAIL>', password: 'invalidpassword' });
-    //     expect(response.status()).toBe(401);
+        const responseCreate = await auth.createUser(user);
+        expect(responseCreate.status()).toBe(201);
+        //ação
+        const response = await auth.login({...user, password: '123456' });
+        expect(response.status()).toBe(401);
 
-    //     const responseBody = await response.json();
-    //     expect(responseBody).toHaveProperty('message', 'Credenciais inválidas.')
-    // })
+        const body = await response.json();
+
+        // const response = await login.auth({ email: '<EMAIL>', password: 'invalidpassword' });
+        // expect(response.status()).toBe(401);
+
+        // const responseBody = await response.json();
+        expect(body).toHaveProperty('message', 'Credenciais inválidas')
+    })
 })      
