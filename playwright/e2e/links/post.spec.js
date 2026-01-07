39be5031803d83da347293914265e26d19d2b580
Edson <edsonj82@outlook.com>
2026-01-07 18:48:37 -0300

    creating scenario: 'should not create a link with invalid url'

--- playwright/e2e/links/post.spec.js
+++ playwright/e2e/links/post.spec.js
@@ -112,4 +112,30 @@ test.describe('POST /links', () => {
         const responseBody = await response.json();
         expect(responseBody).toHaveProperty('message', 'O campo \'Title\' é obrigatório')
     })
+
+    test('should not create a link with invalid url', async ({ request }) => {
+
+        const user = getUser()
+        const link = {
+            original_url: 'invalid-url',
+            title: 'Link inválido'
+        }
+
+        const authorization = authService(request);
+        const links = linkService(request);
+
+        const responseCreate = await authorization.createUser(user);
+        expect(responseCreate.status()).toBe(201);
+
+        const loginResponse = await authorization.login(user);
+        expect(loginResponse.status()).toBe(200);
+
+        const token = await authorization.getToken(user);
+
+        const response = await links.createLink(link, token);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', 'O campo \'OriginalURL\' deve ser uma URL válida')
+    })
 })
