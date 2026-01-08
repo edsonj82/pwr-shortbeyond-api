496df5b74e1b3f34a2b60f88c0dd40277b32a256
Edson <edsonj82@outlook.com>
2026-01-08 10:25:43 -0300

    refactoring link scenarios (post.spec.js)

--- playwright/e2e/links/post.spec.js
+++ playwright/e2e/links/post.spec.js
@@ -6,26 +6,21 @@ import { getLink } from '../../support/factories/link';
 
 test.describe('POST /links', () => {
 
+    const user = getUser()
+    const link = getLink()
+
+    let authorization, links, token
+
+    test.beforeEach(async ({ request }) => {
+        authorization = authService(request);
+        links = linkService(request);
+
+        await authorization.createUser(user);
+        await authorization.login(user);
+        token = await authorization.getToken(user);
+    });
+
     test('should create a new link successfully', async ({ request }) => {
-
-        const user = getUser()
-        const link = getLink()
-
-        const authorization = authService(request);
-        const links = linkService(request);
-
-
-        const responseCreate = await authorization.createUser(user);
-        expect(responseCreate.status()).toBe(201);
-
-        const loginResponse = await authorization.login(user);
-        expect(loginResponse.status()).toBe(200);
-
-        // const token = await authService.getToken(user);
-        // const loginBody = await loginResponse.json(user);
-        // const token = loginBody.data.token;
-
-        const token = await authorization.getToken(user);
 
         const response = await links.createLink(link, token);
         expect(response.status()).toBe(201);
@@ -41,9 +36,6 @@ test.describe('POST /links', () => {
 
     test('should not create a invalid link authentication', async ({ request }) => {
 
-        const link = getLink()
-        const links = linkService(request);
-
         const response = await links.createLink(link, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDFLRUNDS1dQSk4zV0dXMjQ1Q0IxQlI3S0EiLCJleHAiOjE3Njc4ODEzNDAsImlhdCI6MTc2Nzc5NDk0MH0.UDm_6LWvYU0NSdU7KLO_rOLYZkBGoTCL3Lnprj-ANF3');
         expect(response.status()).toBe(401);
 
@@ -52,9 +44,6 @@ test.describe('POST /links', () => {
     })
 
     test('should not create without a link authentication', async ({ request }) => {
-
-        const link = getLink()
-        const links = linkService(request);
 
         const response = await links.createLink(link, '');
         expect(response.status()).toBe(401);
@@ -65,21 +54,9 @@ test.describe('POST /links', () => {
 
     test('the original_url field is required', async ({ request }) => {
 
-        const user = getUser()
         const link = {
             title: 'Link inválido'
         }
-
-        const authorization = authService(request);
-        const links = linkService(request);
-
-        const responseCreate = await authorization.createUser(user);
-        expect(responseCreate.status()).toBe(201);
-
-        const loginResponse = await authorization.login(user);
-        expect(loginResponse.status()).toBe(200);
-
-        const token = await authorization.getToken(user);
 
         const response = await links.createLink(link, token);
         expect(response.status()).toBe(400);
@@ -90,21 +67,9 @@ test.describe('POST /links', () => {
 
     test('the title field is required', async ({ request }) => {
 
-        const user = getUser()
         const link = {
             original_url: 'https://www.invalidlink.com'
         }
-
-        const authorization = authService(request);
-        const links = linkService(request);
-
-        const responseCreate = await authorization.createUser(user);
-        expect(responseCreate.status()).toBe(201);
-
-        const loginResponse = await authorization.login(user);
-        expect(loginResponse.status()).toBe(200);
-
-        const token = await authorization.getToken(user);
 
         const response = await links.createLink(link, token);
         expect(response.status()).toBe(400);
@@ -115,22 +80,10 @@ test.describe('POST /links', () => {
 
     test('should not create a link with invalid url', async ({ request }) => {
 
-        const user = getUser()
         const link = {
             original_url: 'invalid-url',
             title: 'Link inválido'
         }
-
-        const authorization = authService(request);
-        const links = linkService(request);
-
-        const responseCreate = await authorization.createUser(user);
-        expect(responseCreate.status()).toBe(201);
-
-        const loginResponse = await authorization.login(user);
-        expect(loginResponse.status()).toBe(200);
-
-        const token = await authorization.getToken(user);
 
         const response = await links.createLink(link, token);
         expect(response.status()).toBe(400);
@@ -140,20 +93,6 @@ test.describe('POST /links', () => {
     })
 
     test('should not create a duplicate link', async ({ request }) => {
-
-        const user = getUser()
-        const link = getLink()
-
-        const authorization = authService(request);
-        const links = linkService(request);
-
-        const responseCreate = await authorization.createUser(user);
-        expect(responseCreate.status()).toBe(201);
-
-        const loginResponse = await authorization.login(user);
-        expect(loginResponse.status()).toBe(200);
-
-        const token = await authorization.getToken(user);
 
         const responseFirst = await links.createLink(link, token);
         expect(responseFirst.status()).toBe(201);
