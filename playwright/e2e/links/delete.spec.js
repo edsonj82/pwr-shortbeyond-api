391fe9af643815192bc58102c76e1439d4460b18
Edson <edsonj82@outlook.com>
2026-01-20 11:35:41 -0300

    developing scenario 'should not delete a link that belongs to another user' (BUG)

--- playwright/e2e/links/delete.spec.js
+++ playwright/e2e/links/delete.spec.js
@@ -25,7 +25,7 @@ test.describe('DELETE /links/:id', () => {
         const token = await authorization.getToken(user)
 
         const resonse = await links.removeLink('nonexistent-link-id', token)
-        // TODO: BUG - The API is returning 401 instead of 400
+        // TODO: BUG - The API is returning 400 instead of 401
         expect(resonse.status()).toBe(404);
         const body = await resonse.json();
         expect(body).toHaveProperty('message', 'Link não encontrado')
@@ -45,4 +45,20 @@ test.describe('DELETE /links/:id', () => {
         // TODO: BUG - Adjust return message
         expect(body).toHaveProperty('message', 'token is malformed: token contains an invalid number of segments')
     })
+
+    test('should not delete a link that belongs to another user', async ({ authorization, links }) => {
+        const user1 = getUserWithLinks()
+        const user2 = getUserWithLinks()
+        await authorization.createUser(user1);
+        await authorization.createUser(user2);
+        const token2 = await authorization.getToken(user2)
+        const linkId1 = await links.getLinkId(user1.links[0], token2)
+        const resonse = await links.removeLink(linkId1, token2)
+        // TODO: BUG - The API is allowing delete links of the another user
+        // TODO: BUG - The API is returning 200 instead of 403
+        expect(resonse.status()).toBe(403);
+        const body = await resonse.json();
+        expect(body).toHaveProperty('message', 'Você não tem permissão para excluir este link')
+    })
+    
 })
