ab1bb72d648d046dca350f66a5076701083c8261
Edson <edsonj82@outlook.com>
2026-01-22 17:06:27 -0300

    configuring dotenv & centralizing baseURL local

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /**
  * Read environment variables from file.
@@ -27,8 +29,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:3333',
-
+    // baseURL: 'http://localhost:3333',
+    baseURL: process.env.BASE_API,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
